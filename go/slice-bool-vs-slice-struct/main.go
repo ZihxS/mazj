7c2d6e625344b638854a9c09cfd0c15d999f7327
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2025-11-01 18:36:38 +0700

    chore: update

--- go/slice-bool-vs-slice-struct/main.go
+++ go/slice-bool-vs-slice-struct/main.go
@@ -28,15 +28,15 @@ func main() {
 
 	fmt.Print("\n\n\n\n\n")
 
-	fmt.Printf("🌟 JUMLAH ELEMEN -> %d\n\n", N)
+	fmt.Printf("🌟 JUMLAH ELEMEN\n----> %d\n\n", N)
 
-	fmt.Printf("👉 []bool -> %d bytes (kurang lebih %.2f MB)\n", boolUsage, float64(boolUsage)/1e6)
-	fmt.Printf("👉 []struct{} -> %d bytes (kurang lebih %.2f MB)\n\n", structUsage, float64(structUsage)/1e6)
+	fmt.Printf("👉 PAKE []bool\n----> %d bytes (kurang lebih %.2f MB)\n\n", boolUsage, float64(boolUsage)/1e6)
+	fmt.Printf("👉 PAKE []struct{}\n----> %d bytes (kurang lebih %.2f MB)\n\n", structUsage, float64(structUsage)/1e6)
 
 	if boolUsage > 0 {
 		saved := float64(boolUsage-structUsage) / 1e6
 		percent := (1 - float64(structUsage)/float64(boolUsage)) * 100
-		fmt.Printf("✅ HEMAT -> kurang lebih %.2f MB (kurang lebih %.2f%%)\n", saved, percent)
+		fmt.Printf("✅ HEMAT\n----> kurang lebih %.2f MB (kurang lebih %.2f%%)\n", saved, percent)
 	}
 
 	fmt.Print("\n\n\n\n\n")
