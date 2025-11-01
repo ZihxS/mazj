6dfb3a1c39bb10843387265613b08d4d99ed8069
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2025-11-01 17:38:18 +0700

    chore: update for inaccurate string iteration

--- go/inaccurate-string-iteration/for/main.go
+++ go/inaccurate-string-iteration/for/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	str := "こんにちは"
-	for i := 0; i < len(str); i++ {
-		fmt.Printf("Index %d: %c\n", i, str[i])
+	str := []rune("こんにちは")
+	for i := range str {
+		fmt.Printf("%c", str[i])
 	}
 }
